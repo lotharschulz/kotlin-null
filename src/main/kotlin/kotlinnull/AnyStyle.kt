ca11cfddf0f0b78c952febb2e7e088d832305b5e
Lothar Schulz <lothar.sch@gmx.de>
2020-10-06 16:39:02 +0200

    chore. when more readable than if

--- src/main/kotlin/kotlinnull/AnyStyle.kt
+++ src/main/kotlin/kotlinnull/AnyStyle.kt
@@ -2,9 +2,11 @@ package kotlinnull
 
 object AnyStyle {
     fun parse(s: String): Any =
-            if (s.matches(Regex("-?[0-9]+"))) s.toInt()
-            else NumberFormatException("$s is not a valid integer.")
-
+            when(s.matches(Regex("-?[0-9]+"))){
+                true -> s.toInt()
+                false -> NumberFormatException("$s is not a valid integer.")
+            }
+    
     fun reciprocal(i: Int): Any =
             if (i == 0) IllegalArgumentException("Can not take reciprocal of 0.")
             else 1.0 / i
